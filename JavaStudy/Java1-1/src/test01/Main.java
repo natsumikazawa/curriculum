4b5295948b90f4ca682b83f3cbbbdc39941121cf
natsumikazawa <94304945+natsumikazawa@users.noreply.github.com>
2024-04-29 12:42:04 +0900

    Create Main.java

---
+++ JavaStudy/Java1-1/src/test01/Main.java
@@ -0,0 +1,22 @@
+package test01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("1番目に出力します");
+        System.out.println("2番目に出力します");
+        System.out.println("3番目に出力します");
+        System.out.println("4番目に出力します");
+        
+        
+        int x;
+        int y;
+        
+        x = 2;
+        y = 3;
+        
+        System.out.println(x + y);
+        }
+
+}
