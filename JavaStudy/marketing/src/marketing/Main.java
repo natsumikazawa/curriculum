a11a2f249a8013b75c60494cd5a87c794fab1f44
natsumikazawa <94304945+natsumikazawa@users.noreply.github.com>
2024-04-29 12:41:47 +0900

    Create Main.java

---
+++ JavaStudy/marketing/src/marketing/Main.java
@@ -0,0 +1,10 @@
+package marketing;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int[] arr = new int[5];
+        System.out.println(arr[10]); // 配列の範囲外のインデックスにアクセスしてランタイムエラーになる
+    }
+}
