85d04eac63fa803f263182c10d4cdd089ba07efe
natsumikazawa <94304945+natsumikazawa@users.noreply.github.com>
2024-04-29 12:41:59 +0900

    Create Main.java

---
+++ JavaStudy/Java1-1/src/test06/Main.java
@@ -0,0 +1,22 @@
+package test06;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte dataByte1 = 15;
+         int answerInt1;
+         
+         answerInt1 = dataByte1;
+         
+         System.out.println(answerInt1);
+         
+         int dataByte2 = 15;
+         
+         byte answerInt2;
+         
+         answerInt2 = (byte) dataByte2;
+         
+         System.out.println(answerInt2);
+    }
+
+}
